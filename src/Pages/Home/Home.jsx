34cfc0f2e0f3a0660fb2004f9c4587aa3f306a96
MasonNguyen <mason_nguyen@mason.nguyen>
2022-04-04 14:41:30 +0700

    setup project

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import HomeHeader from '../../Components/HomeHeader/HomeHeader';
+
+export default function Home() {
+  return (
+    <div>
+      <HomeHeader />
+    </div>
+  );
+}
